10cf749becf4e146f0c2fdd5a78b98298131998a
Andy Brown <adb1413@rit.edu>
2010-02-04 00:46:54 -0500

    HitRecord for intersection values.

---
+++ applets/HitRecord.java
@@ -0,0 +1,18 @@
+/*
+ * HitRecord.java
+ * 
+ * Author
+ *     Andrew Brown <andrew@andrewdbrown.com>
+ */
+
+
+
+/**
+ * Stores intersection values.
+ */
+public class HitRecord {
+	
+	public double tx0, tx1, txh;
+	public double ty0, ty1, tyh;
+}
+
