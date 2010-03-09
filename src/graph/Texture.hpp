e77984cc470b9765dc583fff21fb3fa6940ec219
Andy Brown <adb1413@rit.edu>
2010-03-08 23:25:50 -0500

    Moved Texture get implementations outside class.

--- src/graph/Texture.hpp
+++ src/graph/Texture.hpp
@@ -24,6 +24,11 @@ using namespace std;
  * @ingroup graph
  * @brief
  *     OpenGL texture node.
+ * 
+ * Base class for textures of various dimensions.
+ * 
+ * @see Texture2D
+ * @see Texture3D
  */
 class Texture : public Node,
                 public Applicable {
@@ -38,13 +43,13 @@ class Texture : public Node,
 		virtual void remove();
 		virtual string toString() const;
 		
-		virtual string getFilename() const {return filename;}
-		virtual GLuint getHandle() {return handle;}
-		virtual string getName() const {return name;}
-		virtual GLenum getType() const {return type;}
-		virtual int getUnit() const {return unit;}
+		virtual string getFilename() const;
+		virtual GLuint getHandle() const;
+		virtual string getName() const;
+		virtual GLenum getType() const;
+		virtual int getUnit() const;
 		
-		static int getNumberOfActiveUnits() {return active_units.size();}
+		static int getNumberOfActiveUnits();
 		static void pause();
 		static void restart();
 	
@@ -60,6 +65,49 @@ class Texture : public Node,
 		virtual void init();
 		virtual void initType() = 0;
 };
+
+
+/**
+ * @return Path to the file this texture was loaded from.
+ */
+inline string Texture::getFilename() const {return filename;}
+
+
+/**
+ * @return OpenGL's unique identifier for this texture.
+ */
+inline GLuint Texture::getHandle() const {return handle;}
+
+
+/**
+ * @return User's unique identifier for this texture.
+ */
+inline string Texture::getName() const {return name;}
+
+
+/**
+ * @return Type of the texture.
+ * 
+ * @note Should be one of:
+ *   @li @c GL_TEXTURE_1D
+ *   @li @c GL_TEXTURE_2D
+ *   @li @c GL_TEXTURE_3D
+ */
+inline GLenum Texture::getType() const {return type;}
+
+
+/**
+ * @return Texture unit holding the texture.
+ * 
+ * @note Intended to be added to @c GL_TEXTURE0 to be used with OpenGL.
+ */
+inline int Texture::getUnit() const {return unit;}
+
+
+/**
+ * @return Number of texture units being used.
+ */
+inline int Texture::getNumberOfActiveUnits() {return active_units.size();}
 
 
 #endif
