1312a61509b1147ebeea2cc2f0ea4d83f34fd188
Andy Brown <adb1413@rit.edu>
2010-03-11 17:23:15 -0500

    Texture3D test changed for textures directory.

--- src/graph/Texture3D.cxx
+++ src/graph/Texture3D.cxx
@@ -22,7 +22,7 @@ int main(int argc,
 	
 	// Handle arguments
 	if (argc == 1)
-		filename = "../../input/bear.vlb";
+		filename = "../../textures/bear.vlb";
 	else if (argc == 2)
 		filename = argv[1];
 	else {
