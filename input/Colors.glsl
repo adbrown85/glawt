6524cd5f1f92b315ce3c3fa3ca6d85118f3e09e3
Andy Brown <adb1413@rit.edu>
2010-03-19 03:46:46 -0400

    Colors.glsl for defines like ORANGE,YELLOW,etc.

---
+++ input/Colors.glsl
@@ -0,0 +1,22 @@
+/*
+ * Colors.glsl
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef COLORS_GLSL
+#define COLORS_GLSL
+
+#define WHITE vec4(1,1,1,1)
+#define BLACK vec4(0,0,0,1)
+#define RED vec4(1,0,0,1)
+#define GREEN vec4(0,1,0,1)
+#define BLUE vec4(0,0,1,1)
+#define YELLOW vec4(1.00,0.90,0.00,1)
+#define ORANGE vec4(1.00,0.65,0.00,1)
+#define PURPLE vec4(0.62,0.13,0.84,1)
+#define BROWN vec4(0.55,0.41,0.08,1)
+#define GRAY vec4(0.5,0.5,0.5,1)
+
+
+#endif
