ed2a4048f7cb96fdd5e30e330b11cc1fee627392
Andy Brown <adb1413@rit.edu>
2010-05-05 07:21:23 -0400

    Cleaned up Matrix and improved times.

--- src/data/Matrix.hpp
+++ src/data/Matrix.hpp
@@ -24,13 +24,11 @@ public:
 	       float b0, float b1, float b2, float b3,
 	       float c0, float c1, float c2, float c3,
 	       float d0, float d1, float d2, float d3);
-	float get(int i, int j) const;
 	float getDeterminant() const;
 	int getColumns() const;
 	Matrix getInverse() const;
 	int getRows() const;
 	int getSize() const;
-	Matrix getSubmatrix(int i, int j) const;
 	Matrix getTranspose() const;
 	float& operator()(int i, int j);
 	float operator()(int i, int j) const;
@@ -40,39 +38,23 @@ public:
 	void set(float array[16]);
 	void toArray(float *array);
 protected:
+	Matrix getSubmatrix(int i, int j) const;
 	float det(int n) const;
 private:
+	Matrix(int size);
 	float arr[4][4];
 	int size;
 };
 
+/** Calculate the determinant of the entire matrix. */
+inline float Matrix::getDeterminant() const {return det(size);}
 
-/**
- * @return an element in the array
- * 
- * @param i Row index
- * @param j Column index
- */
-inline float Matrix::get(int i,
-                         int j) const {
-	return arr[i][j];
-}
+/** @return the number of columns */
+inline int Matrix::getColumns() const {return size;}
 
+/** @return the number of rows. */
+inline int Matrix::getRows() const {return size;}
 
-/**
- * @return the number of columns
- */
-inline int Matrix::getColumns() const {
-	return size;
-}
-
-
-/**
- * @return the number of rows.
- */
-inline int Matrix::getRows() const {
-	return size;
-}
 
 
 
