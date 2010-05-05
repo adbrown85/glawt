34dcefd65d93fa6915ca4822b5936d66151350a9
Andy Brown <adb1413@rit.edu>
2010-05-05 04:56:27 -0400

    NodeListener.

---
+++ src/graph/NodeListener.hpp
@@ -0,0 +1,26 @@
+/*
+ * NodeListener.hpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef NODELISTENER_HPP
+#define NODELISTENER_HPP
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+/**
+ * @ingroup graph
+ * @interface 
+ * @brief Object that is notified by a node.
+ */
+class NodeListener {
+public:
+	virtual void nodeUpdated() = 0;
+};
+
+
+#endif
