775463c587a653857394a13cb2a97d0578ba6266
Andy Brown <adb1413@rit.edu>
2010-05-05 06:48:21 -0400

    Cleaned up Matrix test.

--- src/data/Matrix.cxx
+++ src/data/Matrix.cxx
@@ -5,20 +5,116 @@
  *     Andrew Brown <adb1413@rit.edu>
  */
 #include "Matrix.hpp"
+#include <cassert>
+#include <ctime>
 
 
-int main() {
+class MatrixTest {
+public:
+	void setUp();
+	void testDeterminant();
+	void testInverse();
+	void testSetFromArray();
+	void testTranspose();
+	void testMultiplyOperatorTime();
+	void testVectorMultiply();
+private:
+	float TOLERANCE, ITERATIONS;
+	Matrix m1, m2, m3;
+};
+
+void MatrixTest::setUp() {
+	
+	cout << "Setting up..." << endl;
+	TOLERANCE = 0.000001;
+	ITERATIONS = 100000;
+	m1 = Matrix( 4.0, 3.0, 8.0, 7.0,
+	             3.0, 2.0, 4.0, 6.0,
+	             1.0, 6.0, 4.0, 5.0,
+	             1.0, 2.0, 3.0, 4.0);
+	m1.print();
+}
+
+void MatrixTest::testDeterminant() {
+	
+	float result;
+	
+	cout << "\nTesting determinant..." << endl;
+	result = m1.getDeterminant();
+	cout << "  " << result << endl;
+	assert(result == 69);
+}
+
+void MatrixTest::testInverse() {
+	
+	cout << "\nTesting inverse..." << endl;
+	m2 = m1.getInverse();
+	m2.print();
+	cout << "  ------------------------------------" << endl;
+	m3 = m1 * m2;
+	m3.print();
+	for (int i=0; i<4; ++i)
+		assert(fabs(m3(i,i)-1.0) < TOLERANCE);
+	for (int i=0; i<3; ++i) {
+		for (int j=i+1; j<4; ++j)
+			assert(fabs(m3(i,j)-0.0) < TOLERANCE);
+	}
+	for (int i=1; i<4; ++i) {
+		for (int j=0; j<i; ++j)
+			assert(fabs(m3(i,j)-0.0) < TOLERANCE);
+	}
+}
+
+void MatrixTest::testTranspose() {
+	
+	cout << "\nTesting transpose..." << endl;
+	m3 = m1.getTranspose();
+	m3.print();
+}
+
+void MatrixTest::testVectorMultiply() {
+	
+	Vector v1(1.0, 1.0, 1.0, 1.0), v2;
+	
+	cout << "\nTesting Vector multiply..." << endl;
+	v2 = m2 * v1;
+	cout << "  " << v2 << endl;
+}
+
+void MatrixTest::testSetFromArray() {
 	
 	float arr[16] = { 1.0,  2.0,  3.0,  4.0,
 	                  5.0,  6.0,  7.0,  8.0,
 	                  9.0, 10.0, 11.0, 12.0,
 	                 13.0, 14.0, 15.0, 16.0};
-	Matrix m1( 4.0, 3.0, 8.0, 7.0,
-	           3.0, 2.0, 4.0, 6.0,
-	           1.0, 6.0, 4.0, 5.0,
-	           1.0, 2.0, 3.0, 4.0);
-	Matrix m2, m3;
-	Vector v1(1.0, 1.0, 1.0, 1.0), v2;
+	
+	cout << "\nSetting from array..." << endl;
+	m2.set(arr);
+	m2.print();
+	for (int i=0; i<4; ++i) {
+		for (int j=0; j<4; ++j) {
+			assert(arr[i*4+j] == m2(j,i));
+		}
+	}
+}
+
+void MatrixTest::testMultiplyOperatorTime() {
+	
+	clock_t beg, end;
+	
+	cout << "\nTesting operator* time..." << endl;
+	beg = clock();
+	for (int i=0; i<ITERATIONS; ++i)
+		m1 * m2;
+	end = clock();
+	cout << "  " << (end-beg) << " clocks." << endl;
+	cout << "  " << ((double)end-beg) / CLOCKS_PER_SEC << " seconds." << endl;
+}
+
+
+int main() {
+	
+	MatrixTest test;
 	
 	// Start
 	cout << endl;
@@ -27,38 +123,14 @@ int main() {
 	cout << "****************************************" << endl;
 	cout << endl;
 	
-	// Original
-	cout << "Showing original..." << endl;
-	m1.print();
-	
-	// Determinant
-	cout << "\nCalculating determinant..." << endl;
-	cout << "  " << m1.getDeterminant() << endl;
-	
-	// Inverse
-	cout << "\nCalculating inverse..." << endl;
-	m2 = m1.getInverse();
-	m2.print();
-	
-	// Identity
-	cout << "\nCalculating identity from original and inverse..." << endl;
-	m3 = m1 * m2;
-	m3.print();
-	
-	// Normal
-	cout << "\nCalculating transpose..." << endl;
-	m3 = m1.getTranspose();
-	m3.print();
-	
-	// Vector
-	cout << "\nMultiplying by Vector..." << endl;
-	v2 = m2 * v1;
-	cout << "  " << v2 << endl;
-	
-	// Array
-	cout << "\nSetting from array..." << endl;
-	m2.set(arr);
-	m2.print();
+	// Test
+	test.setUp();
+	test.testDeterminant();
+	test.testInverse();
+	test.testTranspose();
+	test.testVectorMultiply();
+	test.testSetFromArray();
+	test.testMultiplyOperatorTime();
 	
 	// Finish
 	cout << endl;
