c22d68ccdff21771f304c48a4b9a963fb6c79fac
Andy Brown <adb1413@rit.edu>
2010-05-21 07:52:50 -0400

    Moved Slice testing to Dataset.

--- src/data/Dataset.cxx
+++ src/data/Dataset.cxx
@@ -5,30 +5,76 @@
  *     Andrew Brown <adb1413@rit.edu>
  */
 #include <cstring>
-#include <GL/glut.h>
 #include "Dataset.hpp"
+#include "Window.hpp"
+Dataset *dataset=NULL;
+int position, width, height, depth;
 
 
-int main(int argc,
-         char *argv[]) {
+/* GLUT special keys callback. */
+void special(int key, int x, int y) {
 	
-	unsigned char byteValue;
-	GLenum type;
-	float floatValue;
-	Index index;
-	short shortValue;
+	switch(key) {
+	case GLUT_KEY_UP:
+	case GLUT_KEY_RIGHT:
+		if (position == depth-1)
+			position = -1;
+		++position;
+		Window::refresh();
+		break;
+	case GLUT_KEY_DOWN:
+	case GLUT_KEY_LEFT:
+		if (position == 0)
+			position = depth;
+		--position;
+		Window::refresh();
+		break;
+	}
+}
+
+
+/* Draws a slice to the screen. */
+void display(void) {
+	
+	char *data;
+	ostringstream stream;
+	
+	// Clear
+	Window::clear();
+	stream << position;
+	Window::write(stream.str());
+	
+	// Draw pixels
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+		glLoadIdentity();
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+			glLoadIdentity();
+			glRasterPos2f(-1.0, -1.0);
+		glPopMatrix();
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	data = reinterpret_cast<char*>(dataset->getData());
+	data += position * width * height * dataset->getBlock();
+	glDrawPixels(width, height, GL_LUMINANCE, dataset->getType(), data);
+	
+	// Finish
+	Window::flush();
+}
+
+
+int main(int argc, char *argv[]) {
+	
 	string filename;
 	
 	// Handle arguments
-	if (argc == 1)
+	if (argc == 1) {
 		filename = "../../textures/bear.vlb";
-	else if (argc == 5) {
+	} else if (argc == 2) {
 		filename = argv[1];
-		index = Index(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
-	}
-	else {
-		cerr << "Usage: " << argv[0] 
-		     << " [<filename>] [i j k]" << endl;
+	} else {
+		cerr << "Usage: " << argv[0] << " [<filename>]" << endl;
 		exit(1);
 	}
 	
@@ -39,43 +85,25 @@ int main(int argc,
 	cout << "****************************************" << endl;
 	cout << endl;
 	
+	// Initialize
 	try {
-		
-		// Create and print existing
-		Dataset dataset(filename);
-		dataset.print();
-		dataset.print(index);
-		
-		// Assign new value
-		cout << endl;
-		cout << "Assigning new value..." << endl;
-		byteValue = 200;
-		shortValue = 215;
-		floatValue = 230.5;
-		type = dataset.getType();
-		switch (type) {
-			case GL_UNSIGNED_BYTE:
-				dataset.set(index, &byteValue, type);
-				break;
-			case GL_SHORT:
-				dataset.set(index, &shortValue, type);
-				break;
-			case GL_FLOAT:
-				dataset.set(index, &floatValue, type);
-				break;
-		}
-		dataset.print(index);
-	}
-	catch (Exception &e) {
+		dataset = new Dataset(filename);
+		dataset->load();
+		dataset->print();
+		width = dataset->getWidth();
+		height = dataset->getHeight();
+		depth = dataset->getDepth();
+		position = 0;
+	} catch (Exception &e) {
 		cerr << e << endl;
 		exit(1);
 	}
 	
-	// Finish
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Dataset" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
+	// Start the window
+	Window::init(argc, argv);
+	Window::create(filename, dataset->getWidth(), dataset->getHeight());
+	Window::setDisplay(&display);
+	Window::setSpecial(&special);
+	Window::start();
 }
 
