445b05d901787a77d8a83ae6b9c691a7287551e9
Andy Brown <adb1413@rit.edu>
2010-05-28 19:09:57 -0400

    GLEW in configure.

--- src/common.h
+++ src/common.h
@@ -6,11 +6,22 @@
  */
 #ifndef COMMON_H
 #define COMMON_H
+
+// Common includes
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <GL/glew.h>
 #include <GL/gl.h>
+
+// Configuration file
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+// Common defines
+#define GL_GLEXT_PROTOTYPES
+#define ILUT_USE_OPENGL
 
 #endif
