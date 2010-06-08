76721909333f08015c643c5e0f05b55c7290762f
Andy Brown <adb1413@rit.edu>
2010-06-08 01:40:58 -0400

    UniformFloat can link to other nodes.
     - Uses Nameable::search and Node::getAttribute

--- src/basic/UniformFloat.cpp
+++ src/basic/UniformFloat.cpp
@@ -21,6 +21,42 @@ void UniformFloat::apply() {
 }
 
 
+void UniformFloat::associate() {
+	
+	Uniform::associate();
+	
+	Node *node;
+	pair<string,string> linkNameKey;
+	string linkValue;
+	
+	// Doesn't have link
+	if (link.empty())
+		return;
+	
+	// Split up the link
+	linkNameKey = Text::split(link, '.');
+	
+	// Find the link
+	node = Nameable::search(findRoot(this), linkNameKey.first);
+	if (node == NULL) {
+		NodeException e(tag);
+		e << "[UniformVector] Could not find node named '" << link << "'.";
+		throw e;
+	}
+	
+	// Get the value
+	linkValue = node->getAttribute(linkNameKey.second);
+	if (linkValue.empty()) {
+		NodeException e(tag);
+		e << "[UniformVector] Value for '" << link << "' is empty.";
+		throw e;
+	}
+	
+	// Set the value
+	this->value = atof(linkValue.c_str());
+}
+
+
 bool UniformFloat::setAttribute(pair<string,string> attrib) {
 	
 	if (Text::toLower(attrib.first) == "value") {
