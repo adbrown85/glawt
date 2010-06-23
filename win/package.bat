39055000dfa15d67da331375ab3be383473ac8c9
Andy Brown <adb1413@rit.edu>
2010-06-23 04:43:26 -0400

    Changed Windows binary ZIP filename.

--- win/package.bat
+++ win/package.bat
@@ -4,7 +4,7 @@
 set "PACKAGE_DIR=C:\Program Files\Gander"
 set "PACKAGE_TARNAME=gander"
 set "PACKAGE_VERSION=0.95"
-set "ZIPFILE=%PACKAGE_TARNAME%-%PACKAGE_VERSION%-bin.zip"
+set "ZIPFILE=%PACKAGE_TARNAME%-%PACKAGE_VERSION%-win.zip"
 
 :: Zip up directory
 7za a %ZIPFILE% "%PACKAGE_DIR%"
